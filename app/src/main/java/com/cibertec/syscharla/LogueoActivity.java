e27f08c99d29000cfc0506e4e02c780a89197bce
LilyRoxanaLopezMorales <56231928+LilyRoxanaLopezMorales@users.noreply.github.com>
2019-10-24 00:11:45 -0500

    Avence - 01

--- app/src/main/java/com/cibertec/syscharla/LogueoActivity.java
+++ app/src/main/java/com/cibertec/syscharla/LogueoActivity.java
@@ -6,21 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LogueoActivity extends AppCompatActivity implements View.OnClickListener{
     Button btnIngresar;
+    TextView tvRegistrate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logueo);
 
         btnIngresar = (Button)findViewById(R.id.btn_Ingresar);
+        tvRegistrate = (TextView)findViewById(R.id.tv_Registrate);
         btnIngresar.setOnClickListener(this);
+        tvRegistrate.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this,MenuActivity.class);
-        startActivity(intent);
+
+        if(v == btnIngresar) {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }else if(v == tvRegistrate){
+            Intent intent = new Intent(this, Registro1Activity.class);
+            startActivity(intent);
+        }
     }
 }
